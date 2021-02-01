6a16afa1fdc73f8c4eda71ffc9d1e0e599c097f5
am-block <alyssa.montalbano@blockeng.com>
2021-02-01 10:19:48 -0500

    grab badPix in cube and only use those

---
+++ get_blank_pixels.py
@@ -0,0 +1,20 @@
+import scipy.io as sio
+import numpy as np
+import os
+import pickle
+
+if __name__ == "__main__":
+
+    os.chdir('/home/block-am/Documents/SLIC Test Data/')
+
+    data = sio.loadmat('paper_grid')
+    cube = np.array(data['cube']['badPix'][0][0])
+
+    badPix = []
+    for y in range(cube.shape[0]):
+        for x in range(cube.shape[1]):
+            if cube[y][x] != 0.0:
+                badPix.append([y, x])
+
+    with open('badPix.pkl', 'wb') as fp:
+        pickle.dump(badPix, fp)
